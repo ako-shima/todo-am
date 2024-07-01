1d6646078863bcf94500d75c2fdcaee11648f772
ako-shima <a8344212@gmail.com>
2024-07-01 19:20:38 +0800

    Todoの文字を押すと一覧表示画面へ遷移するようにしました

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -16,9 +16,18 @@
         {{-- @if ($loop->odd) --}}
           <div class="flex items-center lg:w-3/5 mx-auto border-b pb-10 mb-10 border-gray-200 sm:flex-row flex-col">
             <div class="sm:w-32 sm:h-32 h-20 w-20 sm:mr-10 inline-flex items-center justify-center rounded-full bg-pink-100 text-pink-500 flex-shrink-0">
-              <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="sm:w-16 sm:h-16 w-10 h-10" viewBox="0 0 24 24">
+              {{-- <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="sm:w-16 sm:h-16 w-10 h-10" viewBox="0 0 24 24">
                 <path d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
+              </svg> --}}
+              
+              @if ($task->image)
+              <img src="{{ asset('storage/' . $task->image) }}" class="object-cover w-full h-full">
+          @else
+              <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="sm:w-16 sm:h-16 w-10 h-10" viewBox="0 0 24 24">
+                  <path d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
               </svg>
+          @endif
+
             </div>
             <div class="flex-grow sm:text-left text-center mt-6 sm:mt-0">
               <h2 class="text-gray-900 text-lg title-font font-medium mb-2">{{ $task->title }}</h2>
