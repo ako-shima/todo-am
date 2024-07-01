094cf701872efef5be4345f5926c622b01c75636
Michi-Tomo <michihiro.ishihara.0926@gmail.com>
2024-07-01 11:54:30 +0800

    Create Taskの見た目を少し変更しました

--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -10,8 +10,8 @@
                 </div >
                 <div class="card-body">
 
-                <p class="card-text">内容：{{ $task->body }}</p>
-                <p>投稿日時：{{ $task->created_at }}</p>
+                <p class="card-text">Content：{{ $task->body }}</p>
+                <p>Date & Time：{{ $task->created_at }}</p>
                 @if($task->image)
                 @endif
         <div class="form-group">
@@ -44,7 +44,7 @@
           </a>
         </div>
         
-               {{-- <div class="flex space-x-4">
+              {{-- <div class="flex space-x-4">
                 <a href="{{ route('tasks.edit', $task->id) }}" class="btn btn-lg"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
                   <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
                   <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z"/>
@@ -64,7 +64,7 @@
                   <path fill-rule="evenodd" d="M10.854 5.146a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7.5 7.793l2.646-2.647a.5.5 0 0 1 .708 0"/>
                   <path d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v13.5a.5.5 0 0 1-.777.416L8 13.101l-5.223 2.815A.5.5 0 0 1 2 15.5zm2-1a1 1 0 0 0-1 1v12.566l4.723-2.482a.5.5 0 0 1 .554 0L13 14.566V2a1 1 0 0 0-1-1z"/>
                 </svg></a>
-               </div> --}}
+              </div> --}}
               </div>
                 </div>
             </div>
